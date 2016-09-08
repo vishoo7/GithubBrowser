b045494b3f9b2cc09b62e53cdd66a5f9edc897cc
Vishal <vishal_agrawal@ymail.com>
2016-09-07 21:58:25 -0400

    Refactor to cleaner

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,24 +2,15 @@ import axios from 'axios'
 
 export const FETCH_REPOSITORY = 'FETCH_REPOSITORY'
 export const FETCH_COMMITS = 'FETCH_COMMITS'
+export const SEARCH_COMMITS = 'SEARCH_COMMITS'
 
-export function fetchRepository(username) {
-  const url = `https://api.github.com/users/${this.props.username}/repos`
-  const request = axios.get(url)
-
-  return {
-    type: FETCH_REPOSITORY,
-    payload: request
-  }
+export const fetchRepository = (username) => { 
+    const payload = axios.get(`https://api.github.com/users/${this.props.username}/repos`)
+    return { type: FETCH_REPOSITORY, payload}
 }
 
-
-export function fetchCommits(username, repo) {
-  const url = `https://api.github.com/repos/${username}/${repo}/commits`
-  const request = axios.get(url)
-
-  return {
-    type: FETCH_COMMITS,
-    payload: request
-  }
+export const fetchCommits = (username, repo) => { 
+    const payload = axios.get(`https://api.github.com/repos/${username}/${repo}/commits`)
+    return { type: FETCH_COMMITS, payload}
 }
+
